2706e11c7776b3c10497bfea217d034dcdd90fda
Kevin Cahalan <kevinacahalan@gmail.com>
2020-12-17 09:37:22 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char ram[65536] = {0};
+
+struct cpu{
+    int reg_1;
+    int reg_2;
+    int reg_3;
+    int reg_4;
+};
+
+
+int main(int argc, char const *argv[]) {
+    /* code */
+    return 0;
+}
